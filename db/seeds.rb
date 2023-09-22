ed0cda7d76c486f016bafce4fdb1398b8ce96165
ZakharDay <zakhar.day@gmail.com>
2023-09-22 15:44:25 +0300

    Sentence seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+raw_text = "Дом Наркомфина — один из знаковых памятников архитектуры советского авангарда и конструктивизма. Построен в 1928—1930 годах по проекту архитекторов Моисея Гинзбурга, Игнатия Милиниса и инженера Сергея Прохорова для работников Народного комиссариата финансов СССР (Наркомфина). Автор замысла дома Наркомфина Гинзбург определял его как «опытный дом переходного типа». Дом находится в Москве по адресу: Новинский бульвар, дом 25, корпус 1. С начала 1990-х годов дом находился в аварийном состоянии, был трижды включён в список «100 главных зданий мира, которым грозит уничтожение». В 2017—2020 годах отреставрирован по проекту АБ «Гинзбург Архитектс», функционирует как элитный жилой дом. Отдельно стоящий «Коммунальный блок» (историческое название) планируется как место проведения публичных мероприятий."
+words = raw_text.downcase.gsub(/[—.—,«»:()]/, '').gsub(/  /, ' ').split(' ')
 
-puts "Hi, we're on Rails"
+# puts words
 
-def generate_color
-  @color = '#'
-  options = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-  6.times { @color += options.sample }
-  @color
+
+100.times do
+  sentence_words = []
+
+  (10..20).to_a.sample.times do
+    sentence_words << words.sample
+  end
+
+  sentence = sentence_words.join(' ').capitalize + '.'
+
+  puts sentence
+  puts "\n\n"
+
 end
-
-100.times { puts generate_color }
