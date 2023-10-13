a01d1aee1ce68e0076f3c0033bb727eaf9965f1d
ZakharDay <zakhar.day@gmail.com>
2023-10-13 17:16:21 +0300

    First turbo frame

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
     @color = '#'
     options = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
     6.times { @color += options.sample }
+
+    @subscription = Subscription.new
   end
 
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_29_135923) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_131112) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "pin_id"
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_29_135923) do
     t.datetime "updated_at", null: false
     t.string "pin_image"
     t.integer "user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
